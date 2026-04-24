fd2a6207fbe5bd3ab926b627ad6ae0b21de24252
Kizo490 <kinzerskiigor@gmail.com>
2026-04-24 11:34:28 +0200

    feat: Adding writing function

--- Database/Database_1.cpp
+++ Database/Database_1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 //Adding 2 basic structures
 struct Entry{
     char Name;
-    char last_name;
-    long phone_number;
+    char Last_Name;
+    long Phone_Number;
 };
 
 struct Base{
@@ -13,6 +13,18 @@ struct Base{
     int maximum_number_of_records;
     Entry* array_of_records;
 };
+//Adding writing function
+void write_out_the_book(Base* base){
+    if (base->current_number_of_records == 0){
+        cout << "Base is empty!" << endl;
+    }
+
+    for(int i = 0; i < base->current_number_of_records; i++){
+        cout << "Name: " << base->current_number_of_records[i].Name << endl;
+        cout << "Last Name: " << base->current_number_of_records[i].Last_Name << endl;
+        cout << "Phone number: " << base->current_number_of_records[i].Phone_Number << endl;
+    }
+}
 
 int main(){
 
