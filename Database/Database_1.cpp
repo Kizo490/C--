4004a479e00384572d8e2ecbb3a7b051dd907467
Kizo490 <kinzerskiigor@gmail.com>
2026-04-24 11:12:07 +0200

    creating a new database

---
+++ Database/Database_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct Entry{
+    char Name;
+    char last_name;
+    long phone_number;
+};
+
+struct Base{
+    int current_number_of_records;
+    int maximum_number_of_records;
+    Entry* array_of_records;
+};
+
+int main(){
+
+}
