1c644c000d63ded8ebb73b0bc371daf9ffde8e21
Kizo490 <kinzerskiigor@gmail.com>
2026-04-27 17:09:41 +0200

    Creating final main function and finish the program

--- Wektory/Vector_1.cpp
+++ Wektory/Vector_1.cpp
@@ -26,6 +26,44 @@ double* add_vectors(const double* vector1, const double* vector2, int size){
 }
 
 int main(){
+    double* v1 = new double[5]{1.0, 2.0, 3.0, 4.0, 5.0};
+    double* v2 = new double[5]{6.0, 7.0, 8.0, 9.0, 10.0};
+
+    double v3[] = {0.0, 2.0, 3.0};
+    double v4[] = {0.0, 7.0, 0.0};
+
+    cout << "--- First pair (Size 5)--- " << endl;
+    cout << " Vector 1: " << endl;
+    print_vector(v1, 5);
+    cout << " Vector 2: " << endl;
+    print_vector(v2, 5);
+
+    double product1 = scalar_product(v1, v2, 5);
+    cout << "Scalar Product: " << product1 << endl;
+
+    double* sum1 = add_vectors(v1, v2, 5);
+    cout << "Sum of vectors: " << endl;
+    print_vector (sum1, 5);
+
+
+    cout << "--- Second pair (Size 3) ---" << endl;
+    cout << " Vector 3: " << endl;
+    print_vector(v3, 3);
+    cout << " Vector 4: " << endl;
+    print_vector(v4, 3);
+
+    double product2 = scalar_product(v3, v4, 3);
+    cout << "Scalar Product: " << product2 << endl;
+
+    double* sum2 = add_vectors(v3, v4, 3);
+    cout << "Sum of vectors: " << endl;
+    print_vector(sum2, 3);
+
+    delete[] v1;
+    delete[] v2;
+
+    delete[] sum1;
+    delete[] sum2;
 
     return 0;
 }
